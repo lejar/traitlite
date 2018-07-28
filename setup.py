91ac10fceeabdf49006f33ca992d6f9880068ed7
Chris Pezley <chris@pezley.net>
2018-07-28 13:37:08 +0100

    Add long description to setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='traitlite',
     version='0.0.1',
@@ -7,6 +10,8 @@ setup(
     author_email='pypi@pezley.net',
     description='A light-weight module that aims to provide control ' \
                 'for class attributes.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/lejar/traitlite',
     packages=find_packages(exclude=['tests']),
     classifiers=[
